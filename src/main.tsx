1126b82790ccb1a99d8849da8024daf64c6e5c2f
Veronchi <72620044+Veronchi@users.noreply.github.com>
2023-05-04 18:00:45 +0300

    feat: add firebase (#3)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import '@/server/firebase';
 import './reset.scss';
 import './normalize.scss';
 import './constants.scss';
