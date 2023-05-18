450d8c64e974b4ddbe25e68caa939395584d551e
Marina Kovel <waveee@gmail.com>
2023-05-18 23:32:46 +0300

    feat: add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,28 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 import { ApolloProvider } from '@apollo/client';
 import { client } from '@/apollo/client';
-import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n/i18n';
 import { App } from './App';
 import { setupStore } from '@/store';
 import '@/server/firebase';
 import './index.scss';
+import { ErrorBoundary } from './components/error-boundary/error-boundary';
 
 const store = setupStore();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <Provider store={store}>
-      <ApolloProvider client={client}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </ApolloProvider>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+          <I18nextProvider i18n={i18n}>
+            <App />
+          </I18nextProvider>
+        </ApolloProvider>
+      </Provider>
+    </ErrorBoundary>
   </StrictMode>
 );
